ed47e3305ade75ee32b2d0ef5801b3cbc6610f41
Parijat <parijat2024@gmail.com>
2024-09-30 06:18:25 -0700

    Ready ALL

--- app/src/main/java/com/example/todoapplication/AppModule.kt
+++ app/src/main/java/com/example/todoapplication/AppModule.kt
@@ -1,7 +1,9 @@
 package com.example.todoapplication
 
 import com.example.todoapplication.data.repositoryimplement.SomeRepositoryImpl
+import com.example.todoapplication.data.repositoryimplement.TaskRepositoryImpl
 import com.example.todoapplication.data.source.SomeRepository
+import com.example.todoapplication.data.source.TaskRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,11 +13,17 @@ import dagger.hilt.android.scopes.ViewModelScoped
 @Module
 @InstallIn(ViewModelComponent::class)
 object AppModule {
-
     @Provides
     @ViewModelScoped
     fun provideSomeRepository(): SomeRepository {
         return SomeRepositoryImpl()
     }
+
+    @Provides
+    @ViewModelScoped
+    fun provideTaskRepository(taskDao: TaskDao): TaskRepository {
+        return TaskRepositoryImpl(taskDao)
+    }
+
 }
 
